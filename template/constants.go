c0fd952acfcfd8224744df6e56317359c0ef4012
navneetprajapati <navneet.p@internshala.com>
2025-04-26 23:34:50 +0530

    working fine

---
+++ template/constants.go
@@ -0,0 +1,9 @@
+package template
+
+// Template paths
+const (
+	// DefaultEmailTemplate is the default path to the email template
+	DefaultEmailTemplate = "tamplets/email_template.html"
+
+	// Add more template paths as needed
+)
